dbb14b62aae266a0cd42b9bf163d793a2a8960e8
legeeksam37 <legeeksam37@gmail.com>
2024-10-31 23:14:57 +0100

    wip video

--- Doppelganger/Assets/Scripts/RemoteVideoControl.cs
+++ Doppelganger/Assets/Scripts/RemoteVideoControl.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -5,18 +6,40 @@ public class RemoteVideoControl : MonoBehaviour
 {
     [SerializeField] VideoPlayer videoPlayer;
     [SerializeField] string videoName;
+
+    //[SerializeField] List<VideoPlayer> videos;
+    [SerializeField] List<string> names; 
+
     bool firstPlay = true;
+    public bool singleVideo;
+
+    int index = 0;
 
 
     public void Play()
     {
+   
         if (firstPlay)
         {
-            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoName);
-            Debug.Log(videoPath);
-            videoPlayer.url = videoPath;
-            videoPlayer.Play();
-            firstPlay = false;
+            if (singleVideo)
+            {
+                string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoName);
+                Debug.Log(videoPath);
+
+                videoPlayer.url = videoPath;
+                videoPlayer.Play();
+                firstPlay = false;
+            }
+            else
+            {
+
+                string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, names[index]);
+                Debug.Log(videoPath);
+                videoPlayer.url = videoPath;
+                videoPlayer.Play();
+                firstPlay = false;
+
+            }
         }
         else
         {
@@ -36,11 +59,30 @@ public class RemoteVideoControl : MonoBehaviour
 
     public void NextVideo()
     {
-        
+
+        if (index >= names.Count)
+        {
+            return;
+        }
+        else
+        {
+           index++;
+            Play();
+        }
+ 
     }
 
     public void PreviousVideo()
-    { 
-    
+    {
+        if (index <= 0)
+        {
+            return;
+        }
+        else
+        {
+            index--;
+            Play();
+        }
+
     }
 }
