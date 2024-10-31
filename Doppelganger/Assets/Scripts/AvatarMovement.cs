a71a283bfef13d633e3f52e06daff09f6b3b572c
legeeksam37 <legeeksam37@gmail.com>
2024-10-31 18:13:38 +0100

    nav mesh wip

--- Doppelganger/Assets/Scripts/AvatarMovement.cs
+++ Doppelganger/Assets/Scripts/AvatarMovement.cs
@@ -5,22 +5,43 @@ using UnityEngine.AI;
 
 public class AvatarMovement : MonoBehaviour
 {
-    NavMeshAgent agent;
     [SerializeField] Transform dest;
+    [SerializeField] float speed;
+
+    NavMeshAgent agent;
+    const string TAG = "AvatarMovement";
+    float dist = 0;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
 
-        if (dest != null)
+        if (agent == null && dest == null)
         {
-            agent.SetDestination(dest.position);
+            Debug.LogError(TAG + " unassigned variables");
+            return;
         }
+
+        Move();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dist = Vector3.Distance(transform.position, dest.position);
+        //Debug.Log(TAG+" distance avatar object : "+dist);
+
+        if (dist <= 2.0f)
+        {
+            agent.isStopped = true;
+            Debug.Log("distance reached");
+        }
+            
+    }
+
+    public void Move()
+    {
+        agent.speed = speed;
+        agent.SetDestination(dest.position);
     }
 }
